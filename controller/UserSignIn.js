4179956e8e5c361b29561d557f795cc7963651ae
“sujal <nemichandsinghal8@gmail.com>
2025-03-03 04:13:47 +0530

    first2 commit

--- controller/UserSignIn.js
+++ controller/UserSignIn.js
@@ -1,5 +1,5 @@
 const User=require('../models/userModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 async function UserSignInController(req,res){
 try{
